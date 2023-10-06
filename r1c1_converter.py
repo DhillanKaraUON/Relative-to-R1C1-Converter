72a479249fe7f5bea3acc889eb22983beba7b5ad
DhillanKaraUON <dhillan.kara@uon.com.au>
2023-10-06 14:00:14 +0800

    Row reference fixed +-

--- r1c1_converter.py
+++ r1c1_converter.py
@@ -25,22 +25,25 @@ def relative(origin,target_cell):
     target_row_int = row(target_cell)
     target_col_int = letter_to_int(col(target_cell))
 
-    difference_row = f"[{target_row_int - origin_row_int}]" if (target_row_int - origin_row_int) != 0 else ""
+    difference_row = f"[{-(target_row_int - origin_row_int)}]" if (-(target_row_int - origin_row_int)) != 0 else ""
     difference_col = f"[{target_col_int - origin_col_int}]" if (target_col_int - origin_col_int) != 0 else ""
 
     return f"R{difference_row}C{difference_col}"
 
 
-relative_formula = "=IFERROR(IF(INT(M275)=INT(M275+J275),M275+J275,IF(INT(M275+J275)=M275+J275,WORKDAY(N275,1,HOLIDAY),N275)),Project_Start)"
+relative_formula = "=IFERROR(IF(INT(M253)=INT(M253+J253),M253+J253,IF(INT(M253+J253)=M253+J253,WORKDAY(N253,1,HOLIDAY),N253)),Project_Start)"
 
 
-origin_cell = "N270"
+origin_cell = "M254"
 
-relative_cells_list = list(set(re.findall(r'[A-Z]+\d+', relative_formula))) #unique list of cells referenced
-print(relative_cells_list)
+abs_cells_list = list(set(re.findall(r'[A-Z]+\d+', relative_formula))) #unique list of cells referenced
+r1c1_cells_list = list(map(lambda cell: relative(cell, origin_cell), abs_cells_list))
+print(origin_cell)
+print(abs_cells_list)
+print(r1c1_cells_list)
 
 r1c1_formula = relative_formula
-for relative_cell_ii in relative_cells_list:
+for relative_cell_ii in abs_cells_list:
     r1c1_cell_ii = relative(relative_cell_ii,origin_cell)
     
     r1c1_formula = r1c1_formula.replace(relative_cell_ii,r1c1_cell_ii)
